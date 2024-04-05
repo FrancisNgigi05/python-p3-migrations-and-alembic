41928757d988708e4aedb3664d3c36ea3c86c738
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-04-05 14:37:50 +0300

    done

---
+++ app/migrations/versions/eb5216eb175f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: eb5216eb175f
+Revises: 
+Create Date: 2024-04-05 14:15:14.282471
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eb5216eb175f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
